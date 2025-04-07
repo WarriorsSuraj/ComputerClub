c81a09c8dddf7fee326183cf2536a6785c764bfb
nx0ly <n9d8sa@gmail.com>
2025-04-07 19:02:30 -0400

    remove url once working since i committed it to public repo

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,6 +2,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv'
 dotenv.config();
 
+// oh yeah migrate this to another db once it works because i just committed it to the repo (its public now so not secure)
 const uri = "mongodb+srv://n9d8sa:N4ACYtKhXTfneO1f@cluster.zd2upok.mongodb.net/?retryWrites=true&w=majority&appName=cluster"; // secure in .env
 
 export const client = new MongoClient(uri, {
